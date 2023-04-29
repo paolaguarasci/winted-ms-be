a7292b0d23b8a1cdd455810903a355beb6d0d416
Paola Guarasci <paolaguarasci@gmail.com>
2023-04-29 17:28:08 +0200

    basic product service crud

--- productservice/src/main/java/it/pingflood/winted/productservice/controller/ProductController.java
+++ productservice/src/main/java/it/pingflood/winted/productservice/controller/ProductController.java
@@ -1,11 +1,20 @@
 package it.pingflood.winted.productservice.controller;
 
+import it.pingflood.winted.productservice.data.dto.ProductPutRequest;
+import it.pingflood.winted.productservice.data.dto.ProductRequest;
+import it.pingflood.winted.productservice.data.dto.ProductResponse;
 import it.pingflood.winted.productservice.service.ProductService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/product")
+@Slf4j
 public class ProductController {
   public final ProductService productService;
   
@@ -13,5 +22,37 @@ public class ProductController {
     this.productService = productService;
   }
   
+  @GetMapping
+  @ResponseStatus(HttpStatus.OK)
+  public ResponseEntity<List<ProductResponse>> getAll() {
+    // FIXME Ha senso fare l'optional di una lista?
+    return ResponseEntity.of(Optional.of(productService.getAll()));
+  }
+  
+  @GetMapping("/{id}")
+  @ResponseStatus(HttpStatus.OK)
+  public ResponseEntity<ProductResponse> getOne(@PathVariable("id") String id) {
+    return ResponseEntity.of(Optional.of(productService.getOne(id)));
+  }
+  
+  @PostMapping
+  @ResponseStatus(HttpStatus.CREATED)
+  public ResponseEntity<ProductResponse> createOne(@RequestBody ProductRequest productRequest) {
+    log.debug("CONTROLLER - Richiesta nuovo prodotto {}", productRequest);
+    return ResponseEntity.of(Optional.of(productService.createProduct(productRequest)));
+  }
+  
+  @PutMapping("/{id}")
+  @ResponseStatus(HttpStatus.OK)
+  public ResponseEntity<ProductResponse> updateOne(@PathVariable("id") String id, @RequestBody ProductPutRequest productRequest) {
+    return ResponseEntity.of(Optional.of(productService.updateProduct(id, productRequest)));
+  }
+  
+  
+  @DeleteMapping("/{id}")
+  @ResponseStatus(HttpStatus.NO_CONTENT)
+  public void deleteOne(@PathVariable("id") String id) {
+    productService.deleteProduct(id);
+  }
   
 }
--- productservice/src/main/java/it/pingflood/winted/productservice/service/ProductService.java
+++ productservice/src/main/java/it/pingflood/winted/productservice/service/ProductService.java
@@ -1,4 +1,22 @@
 package it.pingflood.winted.productservice.service;
 
+import it.pingflood.winted.productservice.data.dto.ProductPutRequest;
+import it.pingflood.winted.productservice.data.dto.ProductRequest;
+import it.pingflood.winted.productservice.data.dto.ProductResponse;
+
+import java.util.List;
+
 public interface ProductService {
+  
+  ProductResponse getOne(String id);
+  
+  List<ProductResponse> getAll();
+  
+  
+  ProductResponse createProduct(ProductRequest productRequest);
+  
+  ProductResponse updateProduct(String id, ProductPutRequest productPutRequest);
+  
+  void deleteProduct(String id);
+  
 }
