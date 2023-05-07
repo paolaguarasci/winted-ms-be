335ea86f1106a24e2bae8d2ed4eb17c4ddeb519b
Paola Guarasci <paolaguarasci@gmail.com>
2023-05-07 19:10:41 +0200

    new event

--- messageservice/src/main/java/it/pingflood/winted/messageservice/handler/NotificationHandler.java
+++ messageservice/src/main/java/it/pingflood/winted/messageservice/handler/NotificationHandler.java
@@ -1,6 +1,6 @@
 package it.pingflood.winted.messageservice.handler;
 
-import it.pingflood.winted.messageservice.event.NewProductEvent;
+import it.pingflood.winted.messageservice.event.*;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -10,9 +10,32 @@ import org.springframework.stereotype.Component;
 @Slf4j
 @RequiredArgsConstructor
 public class NotificationHandler {
-  
   @KafkaListener(id = "foo", topics = "NewProduct")
-  public void handleNotificationTopic(NewProductEvent newProductEvent) {
-    log.info("Ricevuta notifica su prodotto caricato #{}", newProductEvent.getProductId());
+  public void handleNewTopic(NewProductEvent newProductEvent) {
+    log.info("Nuovo prodotto caricato {}", newProductEvent.getProductId());
+  }
+  
+  @KafkaListener(id = "foo", topics = "NewOrder")
+  public void handleNewOrder(NewOrderEvent newOrderEvent) {
+    log.info("L'utente {} ha comprato da {} il prodotto {}", newOrderEvent.getBuyerId(), newOrderEvent.getSellerId(), newOrderEvent.getProductId());
+  }
+  
+  @KafkaListener(id = "foo", topics = "NewReplay")
+  public void handleNewReplay(NewReplayEvent newReplayEvent) {
+    log.info("L'utente {} ha risposto all'utente {}", newReplayEvent.getActor1Id(), newReplayEvent.getActor2Id());
+  }
+  
+  @KafkaListener(id = "foo", topics = "NewOffer")
+  public void handleNewOffer(NewOfferEvent newOfferEvent) {
+    log.info("L'utente {} ha fatto un'offerta all'utente {} per il prodotto {} al prezzo di {}", newOfferEvent.getSellerId(), newOfferEvent.getBuyerId(), newOfferEvent.getProductId(), newOfferEvent.getPrice());
+  }
+  
+  @KafkaListener(id = "foo", topics = "OfferAccepted")
+  public void handleOfferAccepted(OfferAcceptedEvent offerAcceptedEvent) {
+    if (offerAcceptedEvent.getIsAccepted()) {
+      log.info("L'utente {} ha accettato l'offerta dell'utente {} per il prodotto {} al prezzo di {}", offerAcceptedEvent.getBuyerId(), offerAcceptedEvent.getSellerId(), offerAcceptedEvent.getProductId(), offerAcceptedEvent.getPrice());
+    } else {
+      log.info("L'utente {} non ha accettato l'offerta dell'utente {} per il prodotto {} al prezzo di {}", offerAcceptedEvent.getBuyerId(), offerAcceptedEvent.getSellerId(), offerAcceptedEvent.getProductId(), offerAcceptedEvent.getPrice());
+    }
   }
 }
