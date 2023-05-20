96efab60a9a20be8b88e459c0cfeb007ed3d3656
Paola Guarasci <paolaguarasci@gmail.com>
2023-05-20 18:57:30 +0200

    address service init

---
+++ addressservice/src/test/java/it/pingflood/winted/addressservice/AddressserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package it.pingflood.winted.addressservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddressserviceApplicationTests {
+  
+  @Test
+  void contextLoads() {
+  }
+  
+}
