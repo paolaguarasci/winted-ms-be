115b5e814de25cb17077bcea4b1b63b356bf5d8f
Paola Guarasci <paolaguarasci@gmail.com>
2023-06-06 17:08:52 +0200

    rate limiter ok

---
+++ apigateway/src/main/java/it/pingflood/winted/apigateway/config/CircuitBreakerDefaultConfiguration.java
@@ -0,0 +1,22 @@
+package it.pingflood.winted.apigateway.config;
+
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import io.github.resilience4j.timelimiter.TimeLimiterConfig;
+import org.springframework.cloud.circuitbreaker.resilience4j.ReactiveResilience4JCircuitBreakerFactory;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
+import org.springframework.cloud.client.circuitbreaker.Customizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.Duration;
+
+@Configuration
+public class CircuitBreakerDefaultConfiguration {
+  @Bean
+  public Customizer<ReactiveResilience4JCircuitBreakerFactory> defaultCustomizer() {
+    return factory -> factory.configureDefault(id -> new Resilience4JConfigBuilder(id)
+      .circuitBreakerConfig(CircuitBreakerConfig.ofDefaults())
+      .timeLimiterConfig(TimeLimiterConfig.custom().timeoutDuration(Duration.ofMillis(200)).build())
+      .build());
+  }
+}
