f3758861f1d158bce16bd57afdc3e7eb0513deb7
Paola Guarasci <paolaguarasci@gmail.com>
2023-06-06 17:09:28 +0200

    roba spuria

---
+++ apigateway/src/test/java/it/pingflood/winted/apigateway/dto/ProductResponse.java
@@ -0,0 +1,28 @@
+package it.pingflood.winted.apigateway.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class ProductResponse {
+  private String id;
+  private String name;
+  private String description;
+  private BigDecimal price;
+  private Integer preferred;
+  private String brand; // brand-id
+  private String category;
+  private String owner; // owner-id
+  private String featured;
+  private String bought;
+  private String draft;
+  private List<String> resources;
+}
