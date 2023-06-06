b20b793fb5e56baad32f0f7fee75da3f405b2bd4
Paola Guarasci <paolaguarasci@gmail.com>
2023-06-07 00:46:55 +0200

    resource

--- resourceservice/src/main/java/it/pingflood/winted/resourceservice/ResourceserviceApplication.java
+++ resourceservice/src/main/java/it/pingflood/winted/resourceservice/ResourceserviceApplication.java
@@ -1,9 +1,14 @@
 package it.pingflood.winted.resourceservice;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info =
+@Info(title = "Resource API", version = "1.0", description = "Documentation Resource API v1.0")
+)
 public class ResourceserviceApplication {
   
   public static void main(String[] args) {
